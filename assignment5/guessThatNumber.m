af3c7cf2dbd65e370caba98a3b8659f5283ea9b0
Whitesingletonj22 <143100414+Whitesingletonj22@users.noreply.github.com>
2023-11-07 22:41:43 -0500

    Update guessThatNumber.m
    Found 7 bugs, and game runs correctly.

--- assignment5/guessThatNumber.m
+++ assignment5/guessThatNumber.m
@@ -1,8 +1,13 @@
 function guessThatNumber()  % *** WARNING: CONTAINS INTENTIONAL BUGS! ***
+%
+%  Bugs Found (7) + 1 that was left off, but I added to correct the
+%  programmers oversight.
+%
 %---------------------------------------------------------------
 %       USAGE: guessThatNumber() - Assignment 5
 %
-%        NAME:
+%        NAME: John White-Singleton
+%
 %
 % DESCRIPTION: This program is supposed to allow the user to play
 %              the Number Guessing Game, however, it contains bugs.
@@ -42,14 +47,14 @@ fprintf('   3) Advanced (range is 1 to %d)\n', advancedHighest)
 
 level = input('Enter level (1-3): '); 
 
-while level ~= beginner || level ~= moderate || level ~= advanced       
+while level ~= beginner && level ~= moderate && level ~= advanced  %Bug fixed. || was input instead of &&. Users can now make a difficulty selection
 fprintf('Sorry, that is not a valid level selection.\n')
 level = input('Please re-enter a level of play (1-3): ');
 end
 
 % set highest secret number based on level selected
 
-if level == beginner %Bug found: I found this by trying to run the code and getting a incorrect use of '=' error message.                      
+if level == beginner %Bug found. Was found while getting an "incorrect use of '=' error message."                      
 
 highest = beginnerHighest;
 
@@ -58,16 +63,17 @@ elseif level == moderate
 highest = moderateHighest;
 
 else
-highest = advancedhighest;          
+highest = advancedHighest;    % Bug found. Case match error calling variable    
 end
 
 % randomly select secret number between 1 and highest for level of play
 
-secretNumber = floor(rand() + 1 * highest);     
+secretNumber = randi([1, highest]);  % Bug Found: Was unable to have a correct number be generated randomly, so this code is written to select a random number of the given set.
+   
 
 % initialize number of guesses and User_guess
 
-numOfTries = 1;
+numOfTries = 0;
 userGuess = 0;
 
 % repeatedly get user's guess until the user guesses correctly
@@ -79,7 +85,7 @@ while userGuess ~= secretNumber
 
 fprintf('\nEnter a guess (1-%d): ', highest);
 userGuess = input('');
-while userGuess < 1 || userGuess >= highest
+while userGuess < 1 || userGuess > highest % Bug Found. >= would include the highest possible number and elimnate a possible solution.
 
 fprintf('Sorry, that is not a valid guess.\nRe-enter a guess (1-%d): ', highest);
 
@@ -89,25 +95,37 @@ end % of guess validation loop
 
 % add 1 to the number of guesses the user has made
 
+
 numOfTries = numOfTries + 1;
 
 % report whether the user's guess was high, low, or correct
 
-if userGuess > secretNumber
-fprintf('Sorry, %d is too low.\n', userGuess);
-elseif userGuess < secretNumber 
+if userGuess < secretNumber                         %
+fprintf('Sorry, %d is too low.\n', userGuess);      % Bug Found: < and > signs were flipped and needed to be corrected.
+elseif userGuess > secretNumber                     %
 fprintf('Sorry, %d is too high.\n', userGuess);
 elseif numOfTries == 1
 fprintf('\nLucky You!  You got it on your first try!\n\n');
 else
-fprintf('\nCongratulations!  You got %d in %d tries.\n\n', ...
-secretNumber);
+fprintf('\nCongratulations!  You correctly guessed the secret number, %d, and it only took you %d tries.  Your parents must be so proud! \n\n', ...
+secretNumber, numOfTries);  % Bug Found. the program did not display the number of tries it took to guess the number.  added numOfTries for the second %d and it worked.
 
+if numOfTries >= 1 && numOfTries < 4
+    fprintf('Not bad, although there is room for improvement...\n');
+elseif numOfTries >= 4 && numOfTries < 10
+    fprintf('You should practice more...\n');        % Bug Found. The game did not berate the user for their lack of skill.  I made this possible.
+elseif numOfTries >= 10 && numOfTries < 15
+    fprintf('Maybe you should try Tic-Tac-Toe...\n');
+else
+    fprintf('Do NOT quit your day job...\n');
+end
+
+fprintf('')
 
 fprintf('Game Over. Thanks for playing the Guess That Number game.\n\n');
 
 end  % of guessing while loop
 
-end % Bug fixwd by addeding the missing 'end' for the while loop
+end % Bug fixed by addeding the missing 'end' for the while loop
 
 % end of game
